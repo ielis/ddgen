cae0db5b7d739e4987e6728a3482d20586d89044
Daniel Danis <daniel.danis@jax.org>
2020-03-31 13:57:57 -0400

    Refactor `setup.py`, create `requirements.txt`

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
-import io
-
 from setuptools import setup, find_packages
 
 import ddgen
 from ddgen.db.h2 import supported_h2_versions
 
+# read requirements
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+# read description
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
 setup(name='ddgen',
       version=ddgen.__version__,
       packages=find_packages(),
-      install_requires=['pandas>=0.23', 'numpy>=1.16', 'psycopg2-binary>=2.8.3'],
+      install_requires=requirements,
 
-      long_description=io.open('README.md', encoding='utf-8').read(),
+      long_description=long_description,
       long_description_content_type='text/markdown',
 
       author='Daniel Danis',
@@ -20,8 +26,10 @@ setup(name='ddgen',
       license='GPLv3',
       keywords='bioinformatics genomics',
 
-      package_data={'ddgen': ['utils/data/hg19.dict',
-                              'utils/data/hg38.dict'] +
-                             ['db/jar/h2-{}.jar'.format(version) for version in supported_h2_versions]}
+      package_data={
+          '': ['test_data/*'],
+          'ddgen': ['utils/data/hg19.dict',
+                    'utils/data/hg38.dict'] +
+                   ['db/jar/h2-{}.jar'.format(version) for version in supported_h2_versions]}
 
       )
